093e63273c0fa0b728d24d657f9a5ed88aaa3894
arjunpeddi <arjun.boss@gmail.com>
2018-02-13 10:37:07 -0800

    server js

--- index.js
+++ index.js
@@ -29,7 +29,7 @@ var http = require('http');
 http.createServer(function (req, res) {
     
     res.writeHead(200, { 'Content-Type': 'text/html' });
-    res.end('Hello, world!');
+    // res.end('Hello, world!');
 
     (async() => {
     const browser = await puppeteer.launch();
