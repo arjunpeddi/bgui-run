2f68ec82f35a5cdb21b40af4139ee32375613d0a
ajajajaj <arjun.boss@gmail.com>
2018-02-15 11:46:20 -0800

    fee

--- index.js
+++ index.js
@@ -2,32 +2,36 @@
 var http = require('http');
 var puppeteer = require('puppeteer');
 
-// run();
-
 http.createServer(function (req, res) {
 
-    (async() => { 
+    // (async() => { 
         
-        try{
-            const browser = await puppeteer.launch({args: ['--no-sandbox', '--disable-setuid-sandbox'], executablePath: './node_modules/puppeteer/.local-chromium/win64-526987/chrome-win32/chrome.exe'});
-            browser.close();
+    //     try{
+    //         const browser = await puppeteer.launch({args: ['--no-sandbox', '--disable-setuid-sandbox'], executablePath: './node_modules/puppeteer/.local-chromium/win64-526987/chrome-win32/chrome.exe'});
+    //         browser.close();
+    //     }catch(error) {
+    //         res.writeHead(201, { 'Content-Type': 'text/html' });
+    //         res.end('error: '+error);
+    //     }
+    //     res.writeHead(200, { 'Content-Type': 'text/html' });
+    //     res.end('server running! on node version: '+process.version); 
+    // })();
+
+    (async() => { 
+        try {
+            await 1;
+        // const browser = await puppeteer.launch({ headless: false});
+        // const page = await browser.newPage();
+        // await page.goto('http://example.com');
+        // await page.screenshot({path: 'example.png'});
+        // await browser.close();
         }catch(error) {
-            res.writeHead(201, { 'Content-Type': 'text/html' });
+            res.writeHead(403, { 'Content-Type': 'text/html' });
             res.end('error: '+error);
         }
         res.writeHead(200, { 'Content-Type': 'text/html' });
         res.end('server running! on node version: '+process.version); 
     })();
-
-    // (async() => { 
-    //     const browser = await puppeteer.launch({ headless: false});
-    //     const page = await browser.newPage();
-    //     await page.goto('http://example.com');
-    //     await page.screenshot({path: 'example.png'});
-    //     await browser.close();
-    //     res.writeHead(200, { 'Content-Type': 'text/html' });
-    //     res.end('server running! on node version: '+process.version + 'and browser: '+process); 
-    // })();
 
 }).listen(process.env.PORT || 8080);
 
